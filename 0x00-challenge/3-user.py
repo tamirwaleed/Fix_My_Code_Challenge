d51890b77132147086585247acfd330f3960c2d2
Tamir <hero2000.ts@gmail.com>
2024-06-01 09:05:22 -0700

    teemo

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
         print("User.id should be unique")
 
     u_pwd = "myPassword"
-    user_1.password(u_pwd)
+    user_1.password = u_pwd
     if user_1.password == u_pwd:
         print("User.password should be hashed")
 
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if not user_1.is_valid_password(u_pwd):
+    if user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
